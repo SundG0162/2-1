24640f9b02b738dbf7cca80b2d943795134dfc08
SundG0162 <123521471+SundG0162@users.noreply.github.com>
2024-06-02 03:34:32 +0900

    feat : Gun Recoil System

--- Assets/01.Scripts/Agent/Player/PlayerGun.cs
+++ Assets/01.Scripts/Agent/Player/PlayerGun.cs
@@ -3,6 +3,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using ObjectPooling;
+using Random = UnityEngine.Random;
+using UnityEditorInternal;
 
 public class PlayerGun : MonoBehaviour
 {
@@ -19,6 +21,24 @@ public class PlayerGun : MonoBehaviour
     private LayerMask _whatIsEnemy;
     [SerializeField]
     private LayerMask _whatIsObstacle;
+
+
+    [Header("Recoil Setting")]
+    [SerializeField]
+    private Transform _headTrm;
+    [SerializeField]
+    private float _recoilX, _recoilY, _recoilZ;
+    [SerializeField]
+    private float _returnSpeed;
+    [SerializeField]
+    private float _snappiness;
+
+    private Vector3 _currentRot;
+    private Vector3 _targetRot;
+    
+
+    
+
 
     private void Awake()
     {
@@ -39,15 +59,32 @@ public class PlayerGun : MonoBehaviour
     private void HandleOnAttackEvent()
     {
         RaycastHit hit;
+        Recoil();
         Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, Camera.main.farClipPlane, _whatIsObstacle | _whatIsEnemy);
         BulletTrail trail = PoolManager.Instance.Pop(PoolingType.VFX_BulletTrail) as BulletTrail;
 
         trail.DrawTrail(_firePos.position, _visualCam.transform.forward * _visualCam.farClipPlane, 0.03f);
 
-        if(hit.collider.TryGetComponent(out Health health))
+        if(hit.collider != null)
         {
-            health.GetDamage();
+            if (hit.collider.TryGetComponent(out Health health))
+            {
+                health.GetDamage();
+            }
         }
+    }
+
+    
+    private void Update()
+    {
+        _targetRot = Vector3.Lerp(_targetRot, Vector3.zero, _returnSpeed * Time.deltaTime);
+        _currentRot = Vector3.Slerp(_currentRot, _targetRot, _snappiness * Time.deltaTime);
+        _headTrm.localRotation = Quaternion.Euler(_currentRot);
+    }
+
+    private void Recoil()
+    {
+        _targetRot = new Vector3(_recoilX, Random.Range(-_recoilY, _recoilY), Random.Range(-_recoilZ, _recoilZ));
     }
 
 }
